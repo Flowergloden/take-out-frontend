5ee43fa214e9e1f4602352c4f0e4cdb19d9b79fb
FlowerGolden <1327097302@qq.com>
2023-11-14 14:22:23 +0800

    feat: change main window frame to be public

--- take-out-frontend-rider/MainWindow.xaml.cs
+++ take-out-frontend-rider/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.WindowsAppSDK.Runtime.Packages;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -24,8 +25,12 @@ namespace take_out_frontend_rider
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        public Frame MainContextFrame => ContextFrame;
+        public static MainWindow Instance;
+
         public MainWindow()
         {
+            Instance = this;
             this.InitializeComponent();
             ContextFrame.Navigate(typeof(OrderAvailable));
         }
